74ebbbad5459f26a29ac17727a66aadea0fc3b48
Ilya Voloshik <eggestig12353@gmail.com>
2020-09-07 21:00:08 +0300

    1.3 Сreate a platform for further work

---
+++ src/Teams/Controllers/ManageTeamMembersController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Teams.Models;
+namespace Teams.Controllers
+{
+    public class ManageTeamMembersController : Controller
+    {
+
+
+    }
+
+}
---
+++ src/Teams/Controllers/ManageTeamsController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Teams.Models;
+
+namespace Teams.Controllers
+{
+    public class ManageTeamController : Controller
+    {
+
+
+    }
+
+}
