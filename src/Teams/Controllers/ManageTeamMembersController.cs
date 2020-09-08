ee2843a38cfb308f69a24271d6a54302311cdfb4
Ilya Voloshik <eggestig12353@gmail.com>
2020-09-08 16:18:53 +0300

    1) Add default public methods with the views for **created controllers**

--- src/Teams/Controllers/ManageTeamMembersController.cs
+++ src/Teams/Controllers/ManageTeamMembersController.cs
@@ -10,7 +10,21 @@ namespace Teams.Controllers
 {
     public class ManageTeamMembersController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
 
     }
 
--- src/Teams/Controllers/ManageTeamsController.cs
+++ src/Teams/Controllers/ManageTeamsController.cs
@@ -11,8 +11,20 @@ namespace Teams.Controllers
 {
     public class ManageTeamController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
-
 }
