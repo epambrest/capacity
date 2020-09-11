7f14bdd4f7ab780907fb2c29b8c1cac68dab2c27
Y-floryanovich <63396314+Y-floryanovich@users.noreply.github.com>
2020-09-11 16:49:44 +0300

    Application registration was fixed (#27)
    * Update Startup.cs
    
    * Value assignment was removed at all

--- src/Teams/Startup.cs
+++ src/Teams/Startup.cs
@@ -31,7 +31,7 @@ namespace Teams
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
