4b1f3a147953d3b84c800fd3938f55108a1c0565
XD <XD@DESKTOP-M7N8N6F>
2020-09-22 15:57:54 +0300

    without tests

--- src/Teams/Services/ManageTeamsMembersService.cs
+++ src/Teams/Services/ManageTeamsMembersService.cs
@@ -10,28 +10,30 @@ namespace Teams.Services
     public class ManageTeamsMembersService : IManageTeamsMembersService
     {
         private readonly ICurrentUser _currentUser;
-        private readonly ApplicationDbContext db;
-        public ManageTeamsMembersService(ApplicationDbContext context, ICurrentUser currentUser)
+        private readonly IRepository<Team, int> _teamRepository;
+        private readonly IRepository<TeamMember, int> _memberRepository;
+
+        public ManageTeamsMembersService(IRepository<Team, int> teamRepository, IRepository<TeamMember, int> memberRepository, ICurrentUser currentUser)
         {
             _currentUser = currentUser;
-            db = context;
+            _teamRepository = teamRepository;
+            _memberRepository = memberRepository;
         }
 
         [Authorize]
         public void Add(int team_id, string member_id)
         {
-            Team team = db.Team.First(t => t.Id == team_id);
-            if (team.TeamOwner == _currentUser.Current.Id() && !alreadyInTeam(team_id, member_id))
+            Team team = _teamRepository.GetByIdAsync(team_id).Result;
+            if (team.TeamOwner == _currentUser.Current.Id() && !AlreadyInTeam(team_id, member_id))
             {
                 TeamMember member = new TeamMember { TeamId = team_id, MemberId = member_id };
-                db.TeamMembers.Add(member);
-                db.SaveChanges();
+                _memberRepository.InsertAsync(member);
             }
         }
 
-        private bool alreadyInTeam(int team_id, string member_id)
+        private bool AlreadyInTeam(int team_id, string member_id)
         {
-            return db.TeamMembers.Any(t => t.TeamId == team_id && t.MemberId == member_id);
+            return _memberRepository.GetAll().Any(t => t.TeamId == team_id && t.MemberId == member_id);
         }
     }
 }
