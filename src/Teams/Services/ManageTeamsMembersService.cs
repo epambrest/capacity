3e924380e179b3a79aab0e68887ad333a925a9bd
Sherba Kirill <sherba.kirill@gmail.com>
2020-10-07 08:55:13 +0300

    Include

--- src/Teams/Services/ManageTeamsMembersService.cs
+++ src/Teams/Services/ManageTeamsMembersService.cs
@@ -26,7 +26,7 @@ namespace Teams.Services
         
         public async Task<bool> RemoveAsync(int team_id, string member_id)
         {
-            var team = await _teamRepository.GetAll().Where(t => t.Id == team_id).FirstOrDefaultAsync();
+            var team = await _teamRepository.GetAll().Include(t =>t.TeamMembers).Where(t => t.Id == team_id).FirstOrDefaultAsync();
             if (team != null && team.TeamOwner == _currentUser.Current.Id() && MemberInTeam(team,member_id))
             {       
                 return await _teamMemberRepository.DeleteAsync(team.TeamMembers.FirstOrDefault(t => t.MemberId == member_id));
@@ -37,13 +37,6 @@ namespace Teams.Services
         private bool MemberInTeam(Team team, string member_id)
         {
             return team.TeamMembers.Any(t => t.MemberId == member_id);
-        }
-
-        public ManageTeamsMembersService(IRepository<Team, int> teamRepository, IRepository<TeamMember, int> memberRepository, ICurrentUser currentUser)
-        {
-            _currentUser = currentUser;
-            _teamRepository = teamRepository;
-            _teamMemberRepository = memberRepository;
         }
 
         public async Task<bool> AddAsync(int team_id, string member_id)
