449c12f074ebfe07adc4438317a2eaa723217923
Ilya Voloshik <eggestig12353@gmail.com>
2020-10-20 20:58:19 +0300

    Without tests

---
+++ Teams.Tests/SprintRepositoryTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Teams.Data;
+using Teams.Models;
+using Teams.Repository;
+
+namespace Teams.Tests
+{
+    [TestFixture]
+    class SprintRepositoryTest
+    {
+    }
+}
