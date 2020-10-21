0c21d14cf9a4d76da0047efe2d130f1f6e3bb5e9
Ilya <61599600+bkmz12353@users.noreply.github.com>
2020-10-21 12:18:50 +0300

    4.2 Implement sprint table (#63)
    Co-authored-by: Ilya Voloshik <eggestig12353@gmail.com>

---
+++ src/Teams/Models/Sprint.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
+
+namespace Teams.Models
+{
+    public class Sprint
+    {
+        [Key]
+        [Column("Id"), Required, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Column("TeamId"), ForeignKey(nameof(Team)), Required]
+        public int TeamId { get; set; }
+        public virtual Team Team { get; set; }
+        [Column("Name"), Required]
+        public string Name { get; set; }
+        [Column("DaysInSprint"), Required]
+        public int DaysInSprint { get; set; }
+        [Column("StorePointInHours"), Required]
+        public int StorePointInHours { get; set; }
+        [Column("IsActive"), DefaultValue(false)]
+        public bool IsActive { get; set; }
+    }
+}
