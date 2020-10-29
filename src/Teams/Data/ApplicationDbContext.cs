f5fbc370cbf3482e2418ad8e61da6b7af87c438b
aparkhots <skeidar30119222>
2020-10-30 01:17:14 +0300

    4.1 Implement task table

--- src/Teams/Data/ApplicationDbContext.cs
+++ src/Teams/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace Teams.Data
         public DbSet<Team> Team { get; set; }
         public DbSet<TeamMember> TeamMembers { get; set; }
         public DbSet<Sprint> Sprint { get; set; }
+        public DbSet<Teams.Models.Task> Task { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
