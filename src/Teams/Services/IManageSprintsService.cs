f211d03e4fe8ca1efcbe8914f429388d79cb5807
Y-floryanovich <63396314+Y-floryanovich@users.noreply.github.com>
2020-10-31 13:12:47 +0300

    4.3 Implement get all sprints (#70)
    * 4.3 Implement get all sprints
    
    * Implement method GetTeam

---
+++ src/Teams/Services/IManageSprintsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Teams.Models;
+
+namespace Teams.Services
+{
+    public interface IManageSprintsService
+    {
+        Task<IEnumerable<Sprint>> GetAllSprintsAsync(int team_id, DisplayOptions options);
+        Task<Team> GetTeam(int team_id);
+    }
+}
