1e136b9a6b18c01f51a1d4a551bd313f43a13bcd
Artem <35197523+Artem-Halvita@users.noreply.github.com>
2020-11-05 15:38:29 +0300

    Create ManageTaskService

---
+++ src/Teams/Services/ManageTasksService.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Teams.Data;
+
+namespace Teams.Services
+{
+    public class ManageTasksService
+    {
+}
