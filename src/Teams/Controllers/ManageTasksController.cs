3ffd4cb60f54d7358717dd1ab9c525cc622ae047
Artem <35197523+Artem-Halvita@users.noreply.github.com>
2020-11-05 15:47:24 +0300

    Implement GetTasksByIdAsync action method

--- src/Teams/Controllers/ManageTasksController.cs
+++ src/Teams/Controllers/ManageTasksController.cs
@@ -1,16 +1,38 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Teams.Services;
 
 namespace Teams.Controllers
 {
     public class ManageTasksController : Controller
     {
-        public IActionResult Index()
+        private readonly IManageTasksService _manageTasksService;
+        private readonly IAccessCheckService _accessCheckService;
+
+        public ManageTasksController(IManageTasksService manageTasksService, IAccessCheckService accessCheckService)
         {
-            return View();
+            _manageTasksService = manageTasksService;
+            _accessCheckService = accessCheckService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetTaskByIdAsync(int teamId, int taskId)
+        {
+            if (await _accessCheckService.OwnerOrMemberAsync(teamId))
+            {
+                var task = await _manageTasksService.GetTaskByIdAsync(taskId);
+                ViewBag.TaskName = task.Name;
+                return View(task);
+            }
+            else
+            {
+                return View("Error");
+            }
         }
     }
 }
