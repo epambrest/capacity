3041da0380486ef6ea80edfb5c09188f96424ac4
Artem <35197523+Artem-Halvita@users.noreply.github.com>
2020-11-05 15:39:05 +0300

    Implement GetTaskByIdAsync method

--- src/Teams/Services/ManageTasksService.cs
+++ src/Teams/Services/ManageTasksService.cs
@@ -1,3 +1,4 @@
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,4 +9,19 @@ namespace Teams.Services
 {
     public class ManageTasksService
     {
+        private readonly IRepository<Models.Task, int> _repository;
+
+        public ManageTasksService(IRepository<Models.Task, int> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Models.Task> GetTaskByIdAsync(int id)
+        {
+            var task = await _repository.GetAll().Include(t => t.TeamMember)
+                .Include(t => t.TeamMember.Member).Where(t => t.Id == id).FirstOrDefaultAsync();
+
+            return task;
+        }
+    }
 }
