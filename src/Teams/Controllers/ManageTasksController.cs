1ac41a7ad82a83dfd46ec0145c2002be008e921e
Artem <35197523+Artem-Halvita@users.noreply.github.com>
2020-11-09 19:13:21 +0300

    Add check to owner or member

--- src/Teams/Controllers/ManageTasksController.cs
+++ src/Teams/Controllers/ManageTasksController.cs
@@ -23,6 +23,11 @@ namespace Teams.Controllers
         [Authorize]
         public async Task<IActionResult> GetTaskByIdAsync(int teamId, int taskId)
         {
+            if (!await _accessCheckService.OwnerOrMemberAsync(teamId))
+            {
+                return View("ErrorGetTaskById");
+            }
+
             var task = await _manageTasksService.GetTaskByIdAsync(taskId);
             return View(task);
         }
