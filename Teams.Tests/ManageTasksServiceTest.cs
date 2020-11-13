6b1f2ad00139a878773cbde41d07010c377b03f0
Ilya Voloshik <eggestig12353@gmail.com>
2020-11-13 23:33:41 +0300

    Add tests

--- Teams.Tests/ManageTasksServiceTest.cs
+++ Teams.Tests/ManageTasksServiceTest.cs
@@ -100,5 +100,43 @@ namespace Teams.Tests
             // Assert
             Assert.AreEqual(1, result.Id);
         }
+
+        [Test]
+        public async System.Threading.Tasks.Task EditTaskAsync_ManageTaskServiceReturns_True()
+        {
+            //Arrange
+            var task = new Task { Id = 1, TeamId = 1, Name = "Task", MemberId = 1, Link = "vk.com", StoryPoints = 3, SprintId =3 };
+
+            _tasksRepository.Setup(x => x.UpdateAsync(It.IsAny<Task>())).ReturnsAsync(true);
+            _tasksRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(task);
+
+            //Act
+            var result = await _manageTasksService.EditTaskAsync(task);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        public async System.Threading.Tasks.Task EditTaskAsync_ManageSpiritsServiceReturns_False()
+        {
+            //Arrange
+            var task1 = new Task { Id = 1, TeamId = 1, Name = "Ta sk", MemberId = 1, Link = "vk.com", StoryPoints = 3, SprintId = 3 };
+            var task2 = new Task { Id = 1, TeamId = 1, Name = "Task", MemberId = 1, Link = "vkcom", StoryPoints = 3, SprintId = 3 };
+            var task3 = new Task { Id = 1, TeamId = 1, Name = "Task", MemberId = 1, Link = "vk.com", StoryPoints = -3, SprintId = 3 };
+
+            _tasksRepository.Setup(x => x.UpdateAsync(It.IsAny<Task>())).ReturnsAsync(true);
+            _tasksRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(task2);
+
+            //Act
+            var result1 = await _manageTasksService.EditTaskAsync(task1); //Incorrect name
+            var result2 = await _manageTasksService.EditTaskAsync(task2); //Incorrect link count
+            var result3 = await _manageTasksService.EditTaskAsync(task3); //Incorrect SP count
+
+
+            //Assert
+            Assert.IsFalse(result1);
+            Assert.IsFalse(result2);
+            Assert.IsFalse(result3);
+        }
     }
 }
