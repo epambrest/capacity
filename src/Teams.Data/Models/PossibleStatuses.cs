92c7b5b2eb05a93b72e529b98982e64e7bd4b282
Sergey Shep <isergei151@gmail.com>
2020-12-04 21:37:55 +0300

    Fixed value's order and Migrations

--- src/Teams.Data/Models/PossibleStatuses.cs
+++ src/Teams.Data/Models/PossibleStatuses.cs
@@ -6,8 +6,8 @@ namespace Teams.Data.Models
 {
     public class PossibleStatuses
     {
-        public const int createdStatus = 2;
+        public const int createdStatus = 0;
         public const int activeStatus = 1;
-        public const int completedStatus = 0;
+        public const int completedStatus = 2;
     }
 }
