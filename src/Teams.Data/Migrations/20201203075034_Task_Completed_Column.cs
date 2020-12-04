a5b783366ca6097bc462148e84e4495a6d268653
Artem <35197523+Artem-Halvita@users.noreply.github.com>
2020-12-04 12:56:11 +0300

    Delete unnecessary instructions

--- src/Teams.Data/Migrations/20201203075034_Task_Completed_Column.cs
+++ src/Teams.Data/Migrations/20201203075034_Task_Completed_Column.cs
@@ -6,33 +6,11 @@ namespace Teams.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Sprint_Task_TaskId",
-                table: "Sprint");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Sprint_TaskId",
-                table: "Sprint");
-
-            migrationBuilder.DropColumn(
-                name: "StorePointInHours",
-                table: "Sprint");
-
-            migrationBuilder.DropColumn(
-                name: "TaskId",
-                table: "Sprint");
-
             migrationBuilder.AddColumn<bool>(
                 name: "Completed",
                 table: "Task",
                 nullable: false,
                 defaultValue: false);
-
-            migrationBuilder.AddColumn<int>(
-                name: "StoryPointInHours",
-                table: "Sprint",
-                nullable: false,
-                defaultValue: 0);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -40,36 +18,6 @@ namespace Teams.Data.Migrations
             migrationBuilder.DropColumn(
                 name: "Completed",
                 table: "Task");
-
-            migrationBuilder.DropColumn(
-                name: "StoryPointInHours",
-                table: "Sprint");
-
-            migrationBuilder.AddColumn<int>(
-                name: "StorePointInHours",
-                table: "Sprint",
-                type: "int",
-                nullable: false,
-                defaultValue: 0);
-
-            migrationBuilder.AddColumn<int>(
-                name: "TaskId",
-                table: "Sprint",
-                type: "int",
-                nullable: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Sprint_TaskId",
-                table: "Sprint",
-                column: "TaskId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Sprint_Task_TaskId",
-                table: "Sprint",
-                column: "TaskId",
-                principalTable: "Task",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Restrict);
         }
     }
 }
