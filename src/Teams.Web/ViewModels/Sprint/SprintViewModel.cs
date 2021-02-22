4ada8fa30789b005c76a164007af718074916573
Sherba Kirill <sherba.kirill@gmail.com>
2021-02-22 17:35:11 +0300

    moved calculation to service layer

--- src/Teams.Web/ViewModels/Sprint/SprintViewModel.cs
+++ src/Teams.Web/ViewModels/Sprint/SprintViewModel.cs
@@ -17,6 +17,7 @@ namespace Teams.Web.ViewModels.Sprint
         public int Status { get; set; }
         public bool IsOwner { get; set; }
         public List<TaskViewModel> Tasks { get; set; }
-        public string TotalAverageSp { get; set; }
+        public int TotalStoryPoint { get; set; }
+        public double AverageStoryPoint { get; set; }
     }
 }
